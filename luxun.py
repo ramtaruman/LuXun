43b69f28c9962f844aab28b611a2ba6b35df4521
Pokarama <33192787+ramtaruman@users.noreply.github.com>
2018-09-14 01:14:56 +0530

    First creation

---
+++ luxun.py
@@ -0,0 +1,63 @@
+import discord
+import youtube_dl
+
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.voice_client import VoiceClient
+import asyncio
+import time
+
+
+
+Client = discord.Client()
+client = commands.Bot(command_prefix=".")
+
+
+@client.event
+async def on_ready():
+    print("Lu Xun is ready to fight for Wu!")
+
+
+players = {}
+
+
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    player = await voice_client.create_ytdl_player(url)
+
+    players[server.id] = player
+
+    player.start()
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    id = ctx.message.server.id
+    players[id].pause()
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    id = ctx.message.server.id
+    players[id].stop()
+
+@client.command(pass_context=True)
+async def resume(ctx):
+    id = ctx.message.server.id
+    players[id].resume()
+
+
+client.run("insert your token here")
